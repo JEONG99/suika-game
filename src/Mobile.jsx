380c7be1ffbd43b47038dfa26f92eae9881f8390
jeong99 <jbh990621@gmail.com>
2023-12-11 14:31:39 +0900

    fix size

--- src/Mobile.jsx
+++ src/Mobile.jsx
@@ -196,7 +196,7 @@ function Mobile() {
   }, []);
 
   return (
-    <div>
+    <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
       <canvas ref={canvasRef} />
     </div>
   );
